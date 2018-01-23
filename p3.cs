77ce600b588bd9b6603932470efd339557551ac4
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-01-24 01:29:20 +0600

    Update p3.cs

--- p3.cs
+++ p3.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*
+
+If a five-digit number is input through the keyboard, write a program to print a new number
+by adding one to each of its digits. For example if the number that is input is 12391 then the
+output should be displayed as 23402. (Hint: Use the modulus operator ‘%’)
+
+*/
+
+using System;
 
 namespace ConsoleApp4
 {

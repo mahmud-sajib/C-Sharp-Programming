18e834226ae43527ae93d5352c5a4133c3de3ed6
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-02-11 17:45:03 +0600

    Update p1.cs

--- p1.cs
+++ p1.cs
@@ -13,14 +13,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            float a, b, c;
-            Console.WriteLine("please input the salary: \n");
-            a = float.Parse(Console.ReadLine());
-            b = (a*40) / 100;
-            a = a - b;
-            c = (a * 20) / 100;
-            a = a - c;
-            Console.WriteLine("Robin's Gross salary is: " + a);
+            double gross_salary, b, a1, a2;
+
+            Console.WriteLine("Plz input basic salary");
+
+            b = float.Parse(Console.ReadLine());
+
+            a1 = (b * 0.4);
+            a2 = (b * 0.2);
+
+            gross_salary = a1 + a2 + b;
+
+            Console.WriteLine(gross_salary);
         }
     }
 }

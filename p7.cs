4c2adce31c914588dba4c819c2be20b8e85ce015
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-02-11 13:06:14 +0600

    Update p7.cs

--- p7.cs
+++ p7.cs
@@ -7,14 +7,6 @@ namespace Program1b
      
         static void Main(string[] args)
         {
-            /*
-            int a = 1;
-            for(int i = 1; i <= 7; i++)
-             {
-                 Console.WriteLine("{0,7}", a);
-                 a = a * 10;
-             }
-             */
             int a = 1;
             for (int j = 1; j <= 7 ; j++)
             {               

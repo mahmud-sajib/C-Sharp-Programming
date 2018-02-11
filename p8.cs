a4ea2ea4f9c05aeab541eed7775929c7385ba9b9
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-02-11 13:33:53 +0600

    Update p8.cs

--- p8.cs
+++ p8.cs
@@ -6,13 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int a = 10, b = 2;
-            Console.WriteLine(a & b);
-            Console.WriteLine(a | b);
-            Console.WriteLine(a ^ b);
-            Console.WriteLine(a >> b);
-            Console.WriteLine(a << b);
-            Console.WriteLine(~a);
+            int a = 10, b = 2; 
+            Console.WriteLine(a & b); // boolean AND operation 
+            Console.WriteLine(a | b); // boolean OR operation
+            Console.WriteLine(a ^ b); // boolean XOR operation (if a,b both similar result 0 otherwise 1 )
+            Console.WriteLine(~a); // boolean NOT operation
         }
     }
 }

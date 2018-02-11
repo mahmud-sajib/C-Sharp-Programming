b163aab8d1ee22f64b19012c279816e5690b93b9
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-02-11 12:38:15 +0600

    Add files via upload

---
+++ p17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class A
+    {
+        public A(string a)
+        {
+            Console.WriteLine("Hello" + a);
+        }
+    }
+    class B
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A(@" 'str' ");
+        }
+    }
+}

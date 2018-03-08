d9f1658931303f7b49fb290d4edbf961d6861e29
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-03-08 16:18:04 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// pass by value & pass by reference example
+
+using System;
+
+namespace ConsoleApp2
+{
+    
+    class Program
+    {
+        static void passByValue(int value)
+        {
+            value += 5;
+            Console.WriteLine("value inside the function " + value);
+        }
+
+        static void passByRef(ref int value)
+        {
+            value += 5;
+            Console.WriteLine("value inside the function " + value);
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 10;
+            // pass by value
+            Console.WriteLine("value before calling " + value);
+            passByValue(value);
+            Console.WriteLine("value after calling " + value);
+
+            // pass by reference
+            Console.WriteLine("value before calling " + value);
+            passByRef(ref value);
+            Console.WriteLine("value after calling " + value);
+        }
+    }
+}

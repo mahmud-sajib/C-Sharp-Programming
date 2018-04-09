e55926678881fdaa0729d0c744927bd5f4521919
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-04-09 12:37:28 +0600

    Add files via upload

---
+++ true_false_overloading.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        public double grade;
+
+        public Student(double grade)
+        {
+            this.grade = grade;
+        }
+
+        // creating a method for logical operator overloading
+
+        public static bool operator true(Student ob1)
+        {
+            if (ob1.grade != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator false(Student ob1)
+        {
+            if (ob1.grade == 0)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ob1 = new Student(20);
+
+            // comparing two objects to make relational operator overloading
+            if (ob1)
+            {
+                Console.WriteLine("Non Zero result");
+            }
+            else
+            {
+                Console.WriteLine("Results Zero");
+            }
+
+
+
+        }
+    }
+}
